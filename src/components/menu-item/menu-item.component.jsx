d782c85bf9e8446b14c289d94812a30156511fe0
Vaughn Angoy <vaughn.angoy@gmail.com>
2019-11-25 21:11:28 +0000

    adding prettier + formatting

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './menu-item.styles.scss';
 
- export const MenuItem = ({title, imageUrl, size}) => (
+export const MenuItem = ({ title, imageUrl, size }) => (
     <div className={`menu-item ${size}`}>
-        <div className="bg" style={{
-            backgroundImage: `url(${imageUrl})`
-        }}></div>
+        <div
+            className="bg"
+            style={{
+                backgroundImage: `url(${imageUrl})`,
+            }}
+        ></div>
         <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
             <span className="subtitle">Shop Now</span>
